a6dc3e2bf78a5cd1ce24fb2db28610ae391e0a87
Yura Handz <yurahandzv@gmail.com>
2024-12-18 11:22:44 +0200

    adding InputBox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import InputBox from "./components/InputBox"
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl text-black bg-yellow-200'>Some text</h1>
-    </>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(https://www.xe.com/_next/image/?url=https%3A%2F%2Fimages.ctfassets.net%2Fizij9rskv5y1%2F30ypKwIH9vDXHXeLcj9PLY%2Fd1bf5cb4d6e26024e5dc7e845a396226%2FiStock-507216450.jpg&w=3840&q=75)`}}>
+      <InputBox />
+    </div>
   )
 }
 
